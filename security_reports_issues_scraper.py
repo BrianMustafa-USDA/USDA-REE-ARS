0d9f582f6c557bfaec40f426345bec14c2b90b0c
Brian Mustafa <brianmustafa01@gmail.com>
2023-01-04 13:01:34 -0500

    Update security_reports_issues_scraper.py
    Initial Test: Audit log by importing sys module

--- security_reports_issues_scraper.py
+++ security_reports_issues_scraper.py
@@ -2,6 +2,7 @@ import csv
 import configparser
 import json
 import requests
+import sys
 import time
 from github3 import login
 
@@ -32,7 +33,6 @@ print(personal_access_token)
 # Github Login Credential
 github = login(owner, personal_access_token)
 
-
 def log4shell_read_csv_report(file_name):
     log4shell_list_issues = []
     with open(file_name, 'r') as file:
@@ -44,7 +44,6 @@ def log4shell_read_csv_report(file_name):
             # print(row)
     return log4shell_list_issues
 
-
 def weekly_nal_read_csv_report(file_name):
     weekly_nal_list_issues = []
     with open(file_name, 'r') as file:
@@ -54,7 +53,6 @@ def weekly_nal_read_csv_report(file_name):
         for row in csv_reader:
             weekly_nal_list_issues.append(row)
     return weekly_nal_list_issues
-
 
 def ars_bod_read_csv_report(file_name):
     ars_bod_list_issues = []
@@ -66,7 +64,6 @@ def ars_bod_read_csv_report(file_name):
             ars_bod_list_issues.append(row)
             print(row)
     return ars_bod_list_issues
-
 
 def verify_duplicates(list):
     # no_dup_unique_ids_list = []
@@ -80,7 +77,6 @@ def verify_duplicates(list):
         else:
             print("No duplicates")
     print("Total number of duplicates: ", duplicates_count)
-
 
 def log4shell_create_unique_ids(list):
     print("\nUnique IDs: ")
@@ -134,7 +130,6 @@ def weekly_nal_create_unique_ids(list):
     print(assigned_unique_ids_list)
     return assigned_unique_ids_list
 
-
 def ars_bod_create_unique_ids(list):
     print("\nUnique IDs: ")
     print("Length of list: ", len(list))
@@ -156,7 +151,6 @@ def ars_bod_create_unique_ids(list):
         print("Assigned_unique_ids_list (assigned values): ")
         assigned_unique_ids_list.append(pair_assigned_unique_id)
     return assigned_unique_ids_list
-
 
 def log4shell_create_github_issue(title, labels=None, assignees=None, body=None):
     # Create an issue on github.com using the given parameters.
@@ -206,7 +200,6 @@ def log4shell_create_github_issue(title, labels=None, assignees=None, body=None)
         print('Could not create Issue {0:s}'.format(title))
         print('Response: ', new_repo.content)
 
-
 def weekly_nal_create_github_issue(title, labels=None, assignees=None, body=None):
     # Create an issue on github.com using the given parameters.
     # Our url to create issues via POST
@@ -244,7 +237,6 @@ def weekly_nal_create_github_issue(title, labels=None, assignees=None, body=None
     else:
         print('Could not create Issue {0:s}'.format(title))
         print('Response: ', new_repo.content)
-
 
 def ars_bod_create_github_issue(title, labels=None, assignees=None, body=None):
     # Create an issue on github.com using the given parameters.
@@ -292,7 +284,6 @@ def ars_bod_create_github_issue(title, labels=None, assignees=None, body=None):
         print('Could not create Issue {0:s}'.format(title))
         print('Response: ', new_repo.content)
 
-
 def remove_header(list):
     list.pop(0)
     no_header_list = list
@@ -303,22 +294,23 @@ def create_sets(list):
     for i in list.split():
         print(i)
 
-
-def delay_API_requests():
+def delay_api_requests():
     delay_in_sec = int(config['API']['delay'])
     time.sleep(delay_in_sec)
 
-
 # Create hash object from "gitconfig.ini" configuration file
-# to read in weekly csv security report related to "Log4Shell_Weekly NAL (On Prem + Azure + Agents) Vulnerability Report - CHML Vulns  7 Days.csv"
+# to read in weekly csv security report related to "Log4Shell_Weekly NAL (On Prem + Azure + Agents) Vulnerability Report
+# - CHML Vulns  7 Days.csv"
 log4shell_report = config['security-csv-reports']['Log4Shell_report']
 
 # Create hash object from "gitconfig.ini" configuration file
-# to read in weekly csv security reports related to "Weekly NAL (On Prem + Azure + Agents) Vulnerability Report - CHML Vulns  7 Days.csv"
+# to read in weekly csv security reports related to "Weekly NAL (On Prem + Azure + Agents) Vulnerability Report - CHML
+# Vulns  7 Days.csv"
 weekly_nal_report = config['security-csv-reports']['Weekly_NAL_report']
 
 # Create hash object from "gitconfig.ini" configuration file
-# to read in weekly csv reports related to "ARS BOD 22-01 National Agricultural Library (NAL) On-Prem + Azure Scan Report.csv"
+# to read in weekly csv reports related to "ARS BOD 22-01 National Agricultural Library (NAL) On-Prem + Azure Scan
+# Report.csv"
 ars_bod_report = config['security-csv-reports']['ARS_BOD_report']
 
 try:
@@ -344,44 +336,6 @@ try:
     verify_duplicates(log4shell_no_header_issues_list)
     log4shell_no_dupl_all_issues_list = log4shell_create_unique_ids(log4shell_no_header_issues_list)
 
-    '''
-    print("Debug Log4Shell_no_dupl_all_issues list")
-    print(Log4Shell_no_dupl_all_issues_list[0:5])
-    print("Debug Log4Shell_no_dupl_all_issues list")
-    print(Log4Shell_no_dupl_all_issues_list[0:5])
-
-    print("Incremental Test: ")
-    start = 0
-    end = len(Log4Shell_no_dupl_all_issues_list)
-    step = 5
-
-    list_increments = []
-    for i in range(0, len(Log4Shell_no_dupl_all_issues_list), step):
-        x = i
-        print("Debugging x")
-        print(x)
-        list_increments.append(Log4Shell_no_dupl_all_issues_list[x:x + step])
-        # Log4Shell_create_github_issue(unique_id, ["Test Label"], ["brian-mustafa"], unique_ids_list)t
-        # delay_API_requests()
-        print("List of increments: ")
-        list_increments[0:5]
-        print(Log4Shell_no_dupl_all_issues_list[x:x+step])
-
-        unique_id = Log4Shell_no_dupl_all_issues_list[i][0]
-        print(unique_id)
-        print("Unique IDs List (Log4Shell): ")
-        #unique_ids_list = Log4Shell_no_dupl_all_issues_list[i][1]
-        unique_ids_list = Log4Shell_no_dupl_all_issues_list[i][1]
-        print("Unique IDs List: ",unique_ids_list)
-        #Log4Shell_create_github_issue(unique_id, ["Test Label"], ["brian-mustafa"], unique_ids_list)
-        #delay_API_requests()
-    
-    print("New List of increments")
-    print(list_increments)
-    print("Log4Shell_no_dupl_all_issues+list[]")
-    Log4Shell_no_dupl_all_issues_list[x:x + step]
-    
-    '''
     # Iterate through unique identifiers (no duplicates) to pass each issue
     # into Log4Shell_create_github_issue() function
     print("\nCreate issues from Log4Shell Report: ")
@@ -394,9 +348,14 @@ try:
         print("&*Test")
         print(unique_ids_list)
 
-        delay_API_requests()
+        delay_api_requests()
         log4shell_create_github_issue(unique_id, ["Test Label"], ['brian-mustafa'], unique_ids_list)
-
+        """
+        Test:
+        print("sys.audit: ")
+        sys.addaudithook(hook: Callable[[str, tuple]])
+        sys.audit(str, *args)
+        """
     print("\nWeekly NAL Report:")
 
     weekly_nal_issues_list = []
@@ -432,7 +391,7 @@ try:
         print(unique_ids_list)
         print(weekly_nal_no_dupl_all_issues_list[issue][1][0])
 
-        delay_API_requests()
+        delay_api_requests()
 
         log4shell_create_github_issue(unique_id, ["Test Label"], ['brian-mustafa'], unique_ids_list)
 
@@ -440,7 +399,6 @@ try:
 
     ars_bod_issues_list = []
     ars_bod_issues_list = ars_bod_read_csv_report(ars_bod_report)
-
 
     ars_bod_no_header_issues_list = remove_header(ars_bod_issues_list)
 
@@ -464,18 +422,15 @@ try:
         unique_id = ars_bod_no_dupl_all_issues_list[issue][0]
         unique_ids_list = ars_bod_no_dupl_all_issues_list[issue][1]
 
-        delay_API_requests()
+        delay_api_requests()
         ars_bod_create_github_issue(unique_id, ["Test Label"], ['brian-mustafa'], unique_ids_list)
 
 except AttributeError:
     print("Attribute Error.")
 except EOFError:
     print("EOF Error is raised when the input() function hits the end-of-file condition")
-except Exception:
-    print("An unknown error occurred.")
 except FileNotFoundError:
     print("No such file or directory solution.")
-
 except IndentationError:
     print("IndentationError is raised when there is an incorrect indentation.")
 except IndexError:
@@ -486,8 +441,9 @@ except NotImplementedError:
     print("NotImplementedError is raised by abstract methods.")
 except UnboundLocalError:
     print(
-        "Unbound Local Error is raised when a reference is made to a local variable in a function or method but no value has been bound to that variable.")
+        '''Unbound Local Error is raised when a reference is made to a local variable in a function
+        or method but no value has been bound to that variable.''')
 except UnicodeError:
-    print("Unicode Error. Unicode-related encoding or decoding error occured")
+    print("Unicode Error. Unicode-related encoding or decoding error occurred")
 except ZeroDivisionError:
     print("ZeroDivisionError is raised when the second operand of a division or module operation is zero.")
